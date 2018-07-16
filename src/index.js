1b36b585b36cc72fcc0f94cb602cc24d8074c697
Cortney Knorr <contact@cortneyknorr.com>
2018-07-16 17:04:31 +0200

    Added debug logging

--- src/index.js
+++ src/index.js
@@ -1,104 +1,118 @@
-require("@babel/polyfill");
-require("@babel/register");
-require("@babel/plugin-proposal-pipeline-operator");
-require("babel-plugin-transform-function-bind");
+require('@babel/polyfill');
+require('@babel/register');
+require('@babel/plugin-proposal-pipeline-operator');
+require('babel-plugin-transform-function-bind');
+let debug = require('debug')('apollon');
 
-const express = require("express");
-const { execute, subscribe } = require("graphql");
-const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
-const { createServer } = require("http");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const requireDir = require("require-dir");
+const express = require('express');
+const { execute, subscribe } = require('graphql');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { createServer } = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const requireDir = require('require-dir');
 // const { SubscriptionServer } = require("subscriptions-transport-ws");
 // const asyncify = require("callback-to-async-iterator");
 
-const authenticate = require("../other/authentication");
-const formatError = require("./formatError");
-const connectors = requireDir("../connectors");
+const authenticate = require('../other/authentication');
+const formatError = require('./formatError');
+const connectors = requireDir('../connectors');
 
-const {makeExecutableSchema} = require('graphql-tools');
-const rawSchema = require("./schema");
+const { makeExecutableSchema } = require('graphql-tools');
+const rawSchema = require('./schema');
 const schema = makeExecutableSchema(rawSchema);
 
-const corsConfig = require("../config/cors.json");
-const config = require("../config/general.json");
-
+const corsConfig = require('../config/cors.json');
+const config = require('../config/general.json');
 
 const start = async () => {
-  
-  // Initialisation of the connectors
-  for (let connectorName in connectors) {
-    connectors[connectorName] = connectors[connectorName]();
-  }
+	debug('Apollon initializing');
 
-  // Waiting for them to be ready
-  for (let connectorName in connectors) {
-    connectors[connectorName] = await connectors[connectorName];
-  }
+	// Initialisation of the connectors
+	for (let connectorName in connectors) {
+		connectors[connectorName] = connectors[connectorName]();
+	}
 
-  //Generate authentication middleware
-  let authenticateMid = authenticate({connectors, app, config});
-  const app = express();
+	debug('Waiting for connectors to initialize');
+	// Waiting for them to be ready
+	for (let connectorName in connectors) {
+		connectors[connectorName] = await connectors[connectorName];
+	}
 
-  if (process.argv[2] == "dev" || process.env.NODE_ENV == "dev"){
-    console.log("Endpoint /graphiql is accessible");
-    app.use(
-      "/graphiql",
-      cors(corsConfig),
-      bodyParser.json(),
-      graphiqlExpress({
-        endpointURL: config.endpoint || "/"
-        // SubscriptionEndpoint: `ws://localhost:3000/subscriptions`
-      })
-    );
-  }
-  
+	debug('Starting the express app');
+	const app = express();
+	debug('Generating authentication middleware');
+	let authenticateMid = authenticate({ connectors, app, config });
 
-  app.use(config.endpoint || "/",
-    cors(corsConfig),
-    function(request, response, next){
-      authenticateMid(request, next, function(){
-        response.status(401).send();
-      });
-    },
-    bodyParser.json(),
-    graphqlExpress(async (request, res) => {
-      return {
-        context: {
-          connectors,
-          app,
-          request,
-          config
-        },
-        formatError,
-        schema
-      };
-    })
-  );
+	if (process.argv[2] == 'dev' || process.env.NODE_ENV == 'dev') {
+		debug('Starting the GraphIQL endpoint');
+		app.use(
+			'/graphiql',
+			cors(corsConfig),
+			bodyParser.json(),
+			graphiqlExpress({
+				endpointURL: config.endpoint || '/'
+				// SubscriptionEndpoint: `ws://localhost:3000/subscriptions`
+			})
+		);
+		console.log('Endpoint /graphiql is accessible');
+	}
 
+	debug('Starting the main endpoint on %s', config.endpoint || '/');
+	app.use(
+		config.endpoint || '/',
+		cors(corsConfig),
+		bodyParser.json(),
+		function(request, response, next) {
+			authenticateMid(
+				request,
+				() => {
+					debug('Authentication passed: %s', JSON.stringify(request.body));
+					next();
+				},
+				() => {
+					debug('Authentication rejected: %s', JSON.stringify(request.body));
+					response.status(401).send();
+				}
+			);
+		},
+		graphqlExpress(async (request, res) => {
+			return {
+				context: {
+					connectors,
+					app,
+					request,
+					config
+				},
+				formatError,
+				schema
+			};
+		})
+	);
 
-  const server = createServer(app);
-  const PORT = process.env.PORT || 3000;
+	debug('Starting the main endpoint');
+	const server = createServer(app);
+	const PORT = process.env.PORT || 3000;
 
-  server.listen(PORT, () => {
-    console.log(`Apollon server running on port ${PORT}.`);
+	server.listen(PORT, () => {
+		debug('Apollon started on port %s', PORT);
+		console.log(`Apollon server running on port ${PORT}.`);
 
-    // SubscriptionServer.create(
-    //   {
-    //     execute,
-    //     subscribe,
-    //     schema,
-    //     onConnect: async (connectionParams, webSocket) => {
-    //       console.log('webSocket connected');
-    //     }
-    //   },
-    //   {
-    //     server,
-    //     path: "/subscriptions"
-    //   }
-    // );
-  });
+		// SubscriptionServer.create(
+		//   {
+		//     execute,
+		//     subscribe,
+		//     schema,
+		//     onConnect: async (connectionParams, webSocket) => {
+		//       console.log('webSocket connected');
+		//     }
+		//   },
+		//   {
+		//     server,
+		//     path: "/subscriptions"
+		//   }
+		// );
+	});
 };
 
 start();
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,43 +1,49 @@
-const { URL } = require("url");
+const { URL } = require('url');
+let debug = require('debug')('apollon');
 
 // const { PubSub } = require("graphql-subscriptions");
 // const pubsub = new PubSub();
 
-const requireDir = require("require-dir");
-const { Kind } = require("graphql/language");
+const requireDir = require('require-dir');
+const { Kind } = require('graphql/language');
 // const pubsub = require('../pubsub');
 
 class ValidationError extends Error {
-  constructor(message, field) {
-    super(message);
-    this.field = field;
-  }
+	constructor(message, field) {
+		super(message);
+		this.field = field;
+	}
 }
 
 function assertValidLink({ url }) {
-  try {
-    new URL(url);
-  } catch (error) {
-    throw new ValidationError("Link validation error: invalid url.", "url");
-  }
+	try {
+		new URL(url);
+	} catch (error) {
+		throw new ValidationError('Link validation error: invalid url.', 'url');
+	}
 }
 
-let schema = requireDir("../types");
+let schema = requireDir('../types');
+debug('Created the schema for the resolvers from the types file');
 
 schema.Query = {};
 schema.Mutation = {};
+debug('Added the Query, Mutation to the executable schema');
 
-requireDir("../resolvers", {
-  mapValue: function(value, baseName) {
-    return value(schema);
-  }
+requireDir('../resolvers', {
+	mapValue: function(value, baseName) {
+		return value(schema);
+	}
 });
+debug('Included resolver implementations');
 
-if (Object.keys(schema.Query).length == 0){
-  delete schema.Query
+if (Object.keys(schema.Query).length == 0) {
+	delete schema.Query;
+	debug('Removed the empty query field from executable schema');
 }
-if (Object.keys(schema.Mutation).length == 0){
-  delete schema.Mutation
+if (Object.keys(schema.Mutation).length == 0) {
+	delete schema.Mutation;
+	debug('Removed the empty mutation field from executable schema');
 }
 
 module.exports = schema;
