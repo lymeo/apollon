16d41f38686a70f22a2011b675f3ca11bde88978
louis-bertrand <louisbertrand35@gmail.com>
2018-07-17 13:40:30 +0200

    clean up

--- connectors/local.js
+++ connectors/local.js
@@ -1,24 +1,5 @@
-let comments = [
-  {
-    id: "0",
-    content: "Hello world",
-    author: "Louis Bertrand"
-  },
-  {
-    id: "1",
-    content: "Lymeo",
-    author: "Cortney Knorr"
-  },
-  {
-    id: "2",
-    content: "brhibhribir",
-    author: "vebiveivhei"
-  }
-];
-
 module.exports = async function() {
-  
   return {
-    comments
+    
   };
 };
