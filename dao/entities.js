acbd92d50b00d9d91b9890ac76dc3b94752c031d
louis-bertrand <40792147+louis-bertrand@users.noreply.github.com>
2018-07-17 13:53:47 +0200

    Delete entities.js

--- dao/entities.js
+++
@@ -1,9 +0,0 @@
-const requireDir = require("require-dir");
-
-module.exports = function(db) {
-  return requireDir("./entities", {
-    mapValue: function(value, baseName) {
-      return value(db);
-    }
-  });
-};
