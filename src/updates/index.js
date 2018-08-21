0ce2164fae771d056e6d0ea5f60b3586b0dd0df7
Cortney <contact@cortneyknorr.com>
2018-08-21 17:19:35 +0200

    Added warning for the package.json

--- src/updates/index.js
+++ src/updates/index.js
@@ -1,11 +1,14 @@
 console.log("Preparing for update ...");
 const shell = require('shelljs');
 const path = require('path');
+const beautify = require('json-beautify');
 const {version} = require("../../package.json");
 
 let sourcePath = path.join(shell.pwd().toString(), process.argv[2]);
 
-const {version: oldVersion} = require(path.join(sourcePath, "./package.json"));
-console.log(`Upgrading from version ${oldVersion} to ${version}`);
+const oldPackageJson = require(path.join(sourcePath, "./package.json"));
+console.log(`Upgrading from version ${oldPackageJson.version} to ${version}`);
 
-require("./previous")(oldVersion, sourcePath);
+require("./previous")(oldPackageJson.version, sourcePath);
+
+console.log("WARNING: You will need to add your custom dependencies to the package.json")
