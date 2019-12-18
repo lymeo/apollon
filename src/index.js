6369b7a4b5e8b82125a1ec9467a6772f9286e967
Cortney Knorr <contact@cortneyknorr.com>
2019-12-19 00:22:53 +0100

    Pathched the fact that I am an idiot

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
-// import devEnv from "./develop.js"
-// import buildEnv from "./build.js"
-// import prodEnv from "./prod.js"
+import devEnv from "./develop.js"
+import buildEnv from "./build.js"
+import prodEnv from "./prod.js"
 
 
-// const envDictionary = {
-//   dev: "develop",
-//   develop: "develop",
-//   development: "develop",
-//   production: "prod",
-//   prod: "prod",
-//   build: "build"
-// };
+const envDictionary = {
+  dev: "develop",
+  develop: "develop",
+  development: "develop",
+  production: "prod",
+  prod: "prod",
+  build: "build"
+};
 
-// global.ENV = (
-//   process.env.APOLLON_ENV ||
-//   process.env.NODE_ENV ||
-//   "dev"
-// ).toLowerCase();
+global.ENV = (
+  process.env.APOLLON_ENV ||
+  process.env.NODE_ENV ||
+  "dev"
+).toLowerCase();
 
-// if (!envDictionary[global.ENV]) {
-//   global.ENV = "dev";
-// }
-// const { start, setConfig, config } = {
-//     develop: devEnv, 
-//     prod: prodEnv, 
-//     build: buildEnv
-// }[envDictionary[global.ENV]];
+if (!envDictionary[global.ENV]) {
+  global.ENV = "dev";
+}
+const { start, setConfig, config } = {
+    develop: devEnv, 
+    prod: prodEnv, 
+    build: buildEnv
+}[envDictionary[global.ENV]];
 
-// console.log(start);
+console.log(start);
 
-// export { start, setConfig, config }
-
-export default 4
+export { start, setConfig, config }
